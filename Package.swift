448d93e3d86fd1f5622629b43acb89b404da5ed9
Tomoki Aonuma <uasi@uasi.jp>
2025-12-24 21:37:20 +0900

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.2
+// SPDX-License-Identifier: 0BSD
+
+import PackageDescription
+
+let package = Package(
+    name: "voicememosutil",
+    platforms: [
+        .macOS(.v15)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "voicememosutil",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        )
+    ]
+)
---
+++ Sources/voicememosutil/VoiceMemosUtil.swift
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: 0BSD
+
+import ArgumentParser
+
+@main
+struct VoiceMemosUtil: ParsableCommand {
+    mutating func run() throws {
+        print("Hello, world!")
+    }
+}
